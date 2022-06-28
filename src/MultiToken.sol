926fcdc1deadfd79c99e8d4b1b870a3805f25a5f
ashhanai <ashhab.naim@gmail.com>
2022-06-28 16:10:55 +0200

    Move contracts to src directory

--- src/MultiToken.sol
+++ src/MultiToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
 import "./interfaces/Permit20.sol";
 
 library MultiToken {
